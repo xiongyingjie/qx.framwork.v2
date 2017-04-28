2e8b1467247b999cc9b0f62c9fc2ea4409c2e73b
xiongyingjie <xiongyingjie@sina.cn>
2017-04-28 16:52:43 +0800

    test

--- src/Web/Areas/Alipay/Controllers/BaseAliPayController.cs
+++ src/Web/Areas/Alipay/Controllers/BaseAliPayController.cs
@@ -6,7 +6,7 @@ namespace Web.Areas.Alipay.Controllers
     {
         protected const string PLATE_ALIPAY_ACCOUNT = "plate-alipay";
         protected const string DEMO_CHARGE_ACCOUNT = "test-panda";
-        //private IAopClient _aopClient;
+        //private IAopClient _aopClient;;
         //private  const  string SEVER_URL = "https://openapi.alipaydev.com/gateway.do";
         //private const string APP_ID = "2016092800613633";// "2016092800613633";
         //private const string APP_PRIVATE_KEY = "MIICXwIBAAKBgQDJPonqnYdPcf0n9pyGgNe5ZcLBBM0eFs51muu68NPryfVgo79zlMFsA2kI8jXhYLa6rwXK5vXCThaoklsXF+pHrrD+usyOtdWa+KNxfAHm1nv7I5TXFjJv0ovBrRpAr4JSLFdG2Sw1rhAtmnD8DUosKHvkW1KXoOEOPfKoEswAwwIDAQABAoGBAIursPK5hEjaNzZ+TWJ4l8Bf5QwrteS1NXOgIw1qydpzH6+D1oN0cc3yi/qeiFC02/2zLZUGOPkzUzyJ31imy3zbLX/+GpOl4ixvITVdLxRPcIUknc/ZDO6mLvGZtQ/ejzAOmOvk2vjy8mkwBG9ewIHAXmJ2B5byCabjeuam1Q7ZAkEA8Aw/ujkw203DNYO354DSQp19FKP0ICEW8V6wqXYlZrG0s/jN1YHWwf+XdkyzZ8QVo7iigAcmhDHIKk01OCmTdwJBANaeJ4yAYNy1syj9wuu2DO0JwNp5kMRvA4yi/vdprTtvsBtBd7oJq0fgReQ36yHram+q18vCr/ErYdnvg/FeWBUCQQCF4N799n2YIgOYahD8TW1296zWASbbcHkCPyRaLulnH/8/TKlHxbVH10vbD6YTXloPSJ9gthw2KCmR5iOjYhS7AkEAhN+P/sXwslTwYj2R85tXr13tf5XqEiPlH6o+jvFnZjgE4SsMNCsOV0a8HsqcEfkNgatVRXr4sSi5wVMv7j0J7QJBALg2lZ32BhO5izFDsqZaqgtytQmkL5Jdh8zIeS7HXAu78O5SJEt1uHixen4IS3Je/CZVOhhxMahl73MBPcYXkQU=";
