f83675beb4a9f803ae63324ab99a8199a5e6cc78
xiongyingjie <xiongyingjie@sina.cn>
2017-06-07 10:03:18 +0800

    。

---
+++ framwork/Qx.Tools/Models/Form/FormControlConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Qx.Tools.CommonExtendMethods;
+using Qx.Tools.Models.Report;
+
+namespace Qx.Tools.Models.Form
+{
+   public class FormControlConfig: BaseControlConfig
+    {
+       public FormControlConfig(string lable, string id, string name, string value, FormControlType type, string crossWidth, List<DropDownListItem> items, int sequence, string regex, string inputTip, string errorTip) : base(lable, id, name, value, type, crossWidth, items, sequence)
+       {
+           this.regex = regex;
+           input_tip = inputTip;
+           error_tip = errorTip;
+       }
+
+     
+       public string regex { get; set; }
+        public string input_tip { get; set; }
+        public string error_tip { get; set; }
+      
+    }
+}
---
+++ framwork/Qx.Tools/Models/FormControlType.cs
@@ -0,0 +1,15 @@
+﻿namespace Qx.Tools.Models
+{
+    public enum FormControlType
+    {
+        Input = 1,
+        DateTime = 2,
+        TextArea = 3,
+        DropDownList = 4,
+        Button = 5,
+        Tip = 6,
+        RichBox = 7,
+        File = 8
+       
+    }
+}
