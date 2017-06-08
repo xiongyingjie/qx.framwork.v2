4c303cde1b7936e7c0ddf9a58bd2f276b6cd5ff8
xiongyingjie <xiongyingjie@sina.cn>
2017-06-08 16:40:52 +0800

    sync2

---
+++ framwork/Qx.Contents/Models/PageControlTypeEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qx.Contents.Models
+{
+ public   enum PageControlTypeEnum
+    {
+        DropDownList=8,
+        Color =9,
+        Number =10,
+        TextBox =11,
+        TextArea=12,
+        PictureBox=13,
+        Button=14,
+        Label=15,
+        File=16,
+        Datetime =17,
+        Date = 18,
+        Checkbox = 19,
+        RichTextBox = 20
+    }
+}
